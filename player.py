7b0afb3500835d88754e9f192bd945aec771ae6e
iortz <gg.baibo@gmail.com>
2023-04-12 21:41:38 +0800

    bugfix: player receive a none

--- player.py
+++ player.py
@@ -4,6 +4,9 @@ from pydub.playback import play
 class LocalPlayer:
 
     def play_audio(self, file_path):
+        if not file_path:
+            return
+
         audio = AudioSegment.from_file(file_path)
         play(audio)
 
